fe0d8d8643ae3c041ab61a29a0f2b68f845b7b5a
Michał Strzelecki <michasstrzelecki@gmail.com>
2021-01-26 17:26:29 +0100

    write test to check product factory is working

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,11 +3,12 @@
 namespace Tests;
 
 use Illuminate\Foundation\Mix;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
 
     protected function setUp(): void
     {
---
+++ tests/Unit/ProductFactoryTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Product;
+use Tests\TestCase;
+
+class ProductFactoryTest extends TestCase
+{
+    /**
+     * Check product factory creates an item.
+     *
+     * @return void
+     */
+    public function testCreatesProductItem()
+    {
+        $this->assertCount(0, Product::all());
+
+        $product = Product::factory()->create();
+
+        $this->assertCount(1, Product::all());
+        $this->assertSame($product->title, Product::all()->first()->title);
+    }
+}
