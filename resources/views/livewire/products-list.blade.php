dc1bd34f19fae26f3fcd927b3cff9cf5b0701ba8
Michał Strzelecki <michasstrzelecki@gmail.com>
2021-01-27 10:18:13 +0100

    create and use product list lw component

---
+++ resources/views/livewire/products-list.blade.php
@@ -0,0 +1,18 @@
+<div x-data="{ products: {{ $products }} }">
+    <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+        <template x-for="product in products">
+            <div class="flex flex-col rounded overflow-hidden shadow-lg">
+                <img class="w-full" x-bind:src="`https://loremflickr.com/320/240?random=${product.id}`" x-bind:alt="product.title">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2" x-text="product.title"></div>
+                    <p class="text-gray-500 text-base" x-text="product.description"></p>
+                </div>
+                <div class="px-6 py-4 mt-auto flex justify-end">
+                    <button class="bg-white border-2 border-gray-500 hover:border-gray-900 rounded-full px-3 py-2 text-sm font-semibold text-gray-500 hover:text-gray-900">
+                        Add to basket
+                    </button>
+                </div>
+            </div>
+        </template>
+    </div>
+</div>
