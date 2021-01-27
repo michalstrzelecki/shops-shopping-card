a089df2560f3f29ad6a1bfdba6c30d4c4e6f9c21
Michał Strzelecki <michasstrzelecki@gmail.com>
2021-01-27 08:45:27 +0100

    create products seeder

--- database/seeders/CreateProducts.php
+++ database/seeders/CreateProducts.php
@@ -2,19 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CreateProducts extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            CreateProducts::class
-        ]);
+        Product::factory()->count(20)->create();
     }
 }
